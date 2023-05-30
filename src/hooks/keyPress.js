67474375b43a1646d3dd29c9d612eb40bf88256e
yinzhedy <yinzhe.dy@gmail.com>
2023-05-30 10:46:55 -0400

    added in clarifying comments for future use/understanding of keypress.js

--- src/hooks/keyPress.js
+++ src/hooks/keyPress.js
@@ -1,8 +1,13 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 export default function useKeyPress(fn) {
-    useEffect(() => {
-        window.addEventListener('keydown', fn)
-        return () => window.removeEventListener('keydown', fn)
-    }, [fn])
+  useEffect(() => {
+    // Add event listener for keydown event
+    window.addEventListener('keydown', fn);
+    
+    // Cleanup function to remove the event listener when the component unmounts or when the dependency array changes
+    return () => {
+      window.removeEventListener('keydown', fn);
+    };
+  }, [fn]);
 }
